0d6c5fa5bf802ba655ded547078e94a74df79c45
Ian Abbott <ian.j.abbott@accenture.com>
2024-08-16 11:18:02 -0500

    switch over to temurin jdk

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,7 @@ set -exv
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 IMAGE_REPO="quay.io"
-ORG="cloudservices"
+ORG="urbanos"
 APP="ubi-trino"
 IMAGE="${IMAGE_REPO}/${ORG}/${APP}"
 IMAGE_TAG=$(${SCRIPT_DIR}/get_image_tag.sh)
@@ -20,8 +20,19 @@ fi
 #     exit 1
 # fi
 
-changed=$(git diff --name-only ^HEAD~1|| egrep -v deploy/clowdapp.yaml) # do not build if only the `deploy/clowdapp.yaml` file has changed
-if [ -n "$changed" ]; then
+# Create tmp dir to store data in during job run (do NOT store in $WORKSPACE)
+export TMP_JOB_DIR=$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")
+echo "job tmp dir location: $TMP_JOB_DIR"
+
+function job_cleanup() {
+    echo "cleaning up job tmp dir: $TMP_JOB_DIR"
+    rm -fr $TMP_JOB_DIR
+}
+
+trap job_cleanup EXIT ERR SIGINT SIGTERM
+
+#changed=$(git diff --name-only ^HEAD~1|| egrep -v deploy/clowdapp.yaml) # do not build if only the `deploy/clowdapp.yaml` file has changed
+#if [ -n "$changed" ]; then
     # docker is used on the RHEL7 nodes
     # DOCKER_CONF="$PWD/.docker"
     # mkdir -p "$DOCKER_CONF"
@@ -32,10 +43,21 @@ if [ -n "$changed" ]; then
     # docker --config="$DOCKER_CONF" logout
 
     # podman is used on the RHEL8 nodes
-    podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-    # podman login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
-    podman build -t "${IMAGE}:${IMAGE_TAG}" ${SCRIPT_DIR}
-    podman push "${IMAGE}:${IMAGE_TAG}"
-    podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
-    podman push "${IMAGE}:latest"
-fi
+#    podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+#    # podman login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+#    podman build -t "${IMAGE}:${IMAGE_TAG}" ${SCRIPT_DIR}
+#    podman push "${IMAGE}:${IMAGE_TAG}"
+#    podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+#    podman push "${IMAGE}:latest"
+
+  DOCKER_CONF="$TMP_JOB_DIR/.docker"
+  mkdir -p "$DOCKER_CONF"
+  docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+  docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" ${SCRIPT_DIR} --progress=plain --no-cache
+  docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+
+  docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+  docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+
+  docker --config="$DOCKER_CONF" logout
+#fi
